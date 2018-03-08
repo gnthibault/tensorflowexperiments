a023f5e73f2859ae3be80d490f678ac03474aef9
gnthibault <gnthibault@gmail.com>
2018-03-08 09:05:59 +0100

    Now added a slightly more interesting example of use of the logging system, more to come

--- simplePOF/graphViz/test.py
+++ simplePOF/graphViz/test.py
@@ -19,27 +19,45 @@ with tf.name_scope("MyLayerGroup"):
         c0 = tf.add_n([b1, b2, b3, b4, b5])
         c0 = tf.multiply(b0,c0)
 
-summary_nn = tf.summary.merge_all()
+summaryOp = tf.summary.merge_all()
 
 with tf.Session() as sess:
-    trainWriter = tf.summary.FileWriter("OutputLogDirectory", graph=sess.graph)
+    trainWriter = tf.summary.FileWriter("OutputLogDirectory/train", graph=sess.graph)
+    testWriter = tf.summary.FileWriter("OutputLogDirectory", graph=sess.graph)
 
     la0 = np.random.rand(5)
     la1 = np.random.rand(5)
   
     for step in range(10):
+        if step % 2 == 0:  # Record summaries and test-set accuracy
+            summary, res = sess.run([summaryOp, c0],
+                                    feed_dict={a0:la0, a1:la1})#test feed
+            testWriter.add_summary(summary, step)
+            print('Value of c0 is {}'.format(res))
+        else:  # Record train set summaries, and train
+            if step % 5 == 0:  # Record execution stats
+                # collect variable of interest + summary + metadata
+                run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+                run_metadata = tf.RunMetadata()
+                summary, res = sess.run([summaryOp, c0],
+                                        feed_dict={
+                                                   a0:la0*step,
+                                                   a1:la1*step},# train feed
+                                        options=run_options,
+                                        run_metadata=run_metadata)
+                print('Value of c0 is {}'.format(res))
 
-        # collect variable of interest + summary
-        summary, val = sess.run([summary_nn, c0], feed_dict={a0:la0*step,a1:la1*step})
-        print('Value of c0 is {}'.format(val))
+                # also collect train metadata
+                trainWriter.add_run_metadata(run_metadata, 'step: {}'.format(step)) 
+                trainWriter.add_summary(summary, step)
+            else:  # Record a summary
+                summary, _ = sess.run([summaryOp, c0],
+                                      feed_dict={a0:la0, a1:la1}) # train feed
+                trainWriter.add_summary(summary, step)
 
-        # also collect train metadata
-        run_metadata = tf.RunMetadata()
-        trainWriter.add_run_metadata(run_metadata, '{}'.format(step)) 
-        trainWriter.add_summary(summary,step)
 
     trainWriter.close()
-
+    testWriter.close()
 
 #At the end of the run, you can lauch
 #tensorboard --logdir=OutputLogDirectory
