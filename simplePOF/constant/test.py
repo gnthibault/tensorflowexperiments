63bef6c3739827c3fa03d5acd6e20b7607648165
gnthibault <gnthibault@gmail.com>
2018-03-27 07:22:36 +0200

    Added some small example of graph exploration

--- simplePOF/constant/test.py
+++ simplePOF/constant/test.py
@@ -4,8 +4,26 @@ import tensorflow as tf
 x = tf.constant(value=[10,20,30], name='x')
 y = tf.Variable(x+1, name='y')
 
-initializer = tf.global_variables_initializer()
+initializer_op = tf.global_variables_initializer()
 
 with tf.Session() as sess:
-  sess.run(initializer)
+  sess.run(initializer_op)
   print('Final value of y is {}'.format(sess.run(y)))
+  print('Another way to compute y is {}'.format(y.eval()))
+
+  # explore collections of variables
+  print('One can list the collections of the graph: {}'.format(
+      sess.graph.get_all_collection_keys()))
+  print('And print the list of variables from a specific collection: {}'.format(
+      sess.graph.get_collection('variables')))
+  print('One can also get any tensor by its name: {}'.format(
+      sess.graph.get_tensor_by_name('x:0')))
+
+  # explore ops
+  print('More generally, one can get a list of all operations of the graph '
+      '{}'.format(sess.graph.get_operations()))
+  print('And then fetch an operation by its name {}'.format(
+      sess.graph.get_operation_by_name('x')))
+  op = sess.graph.get_operation_by_name('x')
+  print('And eventually get inputs: {} outputs: {} or some infos out of an '
+      'operation: {}'.format(op.inputs, op.outputs, op.get_attr('dtype')))
